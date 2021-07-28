88b0c80529f3299b703d8c5fca141957e23a10f0
Gibson-Gichuru <gichuru.developer@gmail.com>
2021-07-28 21:52:23 +0300

    Project Restucture

---
+++ app/mpesa.py
@@ -0,0 +1,10 @@
+## flask related imports
+from flask import current_app
+
+## Python Standard imports
+import base64
+import requests
+
+def access_token(consumer_key, consumer_secret):
+
+    pass
